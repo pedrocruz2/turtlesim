8f990860b43b92eae3328e4667a379bcab2d904d
pedrocruz2 <pedro.cruz@sou.inteli.edu.br>
2024-05-03 02:35:03 -0300

    setup inicial workspace

---
+++ src/desenhador/desenhador/desenhotrue.py
@@ -0,0 +1,2 @@
+def desenhar():
+    print('desenhar')
---
+++ src/desenhador/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'desenhador'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='seu-nome',
+    maintainer_email='seu-email',
+    description='sua-descrição',
+    license='CC0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "teste = desenhador.hello:main",
+            "teste2 = desenhador.goodbye:teste",
+            "desenho = desenhador.desenhotrue:desenhar",
+        ],
+    },
+)
